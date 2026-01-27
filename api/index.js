d68b146fd7742f20ca8e2fd289859c41c5fe5bce
Octaleads <octaleads@example.com>
2026-01-27 19:39:34 +0000

    Fix API handler: Export Express app directly for Vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 // Vercel serverless function handler
 import app from '../server/app.js';
 
-// Export as default handler for Vercel
-export default (req, res) => {
-  return app(req, res);
-};
+// Export Express app directly for Vercel
+export default app;
